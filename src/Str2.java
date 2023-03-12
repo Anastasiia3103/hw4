897fa0379d8c9f5525dc9d7492c08d560b4d2e01
Anastasiia <lavrentjeva97@gmail.com>
2023-03-12 21:23:28 +0100

    Initial commit

---
+++ src/Str2.java
@@ -0,0 +1,74 @@
+public class Str2 {
+    public static String repeatLastNChars (String str, int n) {
+        if ( n < 0 || n > str.length() ) {
+            throw new IllegalArgumentException("n must be between 0 and the length of the string, inclusive.");
+        }
+        String lastNChars = str.substring(str.length() - n);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(lastNChars);
+        }
+        return sb.toString();
+    }
+
+    public static boolean prefixAppearsElsewhere (String str, int N) {
+        String prefix = str.substring(0, N);
+        for (int i = N; i <= str.length() - N; i++) {
+            if ( str.substring(i, i + N).equals(prefix) ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isXYBalanced (String s) {
+        int xCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if ( s.charAt(i) == 'x' ) {
+                xCount++;
+            }
+            else if ( s.charAt(i) == 'y' ) {
+                xCount--;
+            }
+        }
+        return xCount == 0;
+    }
+
+    public static String stringSplosion (String str, int n) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str.substring(0, i + 1));
+        }
+        return result.toString();
+    }
+
+    String input = "Hello";
+    int n = 3;
+    String output = stringSplosion(input, n);
+
+    public static String mergeStrings (String a, String b) {
+        int lenA = a.length();
+        int lenB = b.length();
+        int maxLen = Math.max(lenA, lenB);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < maxLen; i++) {
+            if ( i < lenA ) {
+                sb.append(a.charAt(i));
+            }
+            if ( i < lenB ) {
+                sb.append(b.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    String a = "Hello";
+    String b = "world!";
+    String result = mergeStrings(a, b);
+
+    public void main (String[] args) {
+        System.out.println(output);
+        System.out.println(result);
+    }
+
+}
---
+++ src/Str3.java
@@ -0,0 +1,56 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Str3 {
+    public static int sumOfDigits (String str) {
+        int sum = 0;
+        for (int i = 0; i < str.length (); i++) {
+            char c = str.charAt (i);
+            if ( Character.isDigit (c) ) {
+                int digit = Integer.parseInt (String.valueOf (c));
+                sum += digit;
+            }
+        }
+        return sum;
+    }
+
+    public static int sumNumbers (String str) {
+        // Define a regular expression to match numbers
+        String regex = "\\d+";
+
+        // Compile the regular expression into a pattern
+        Pattern pattern = Pattern.compile (regex);
+
+        // Use the pattern to create a matcher for the input string
+        Matcher matcher = pattern.matcher (str);
+
+        int sum = 0;
+        // Iterate through the matches and add their integer value to the sum
+        while (matcher.find ()) {
+            String match = matcher.group ();
+            int num = Integer.parseInt (match);
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main (String[] args) {
+        String input = "abc123def456ghi789";
+        int sum = sumNumbers (input);
+        System.out.println (sum); // Output: 1368
+
+    }
+
+    public int countYZ (String str) {
+        int count = 0;
+        str = str.toLowerCase () + " "; // convert the string to lowercase and add a space at the end
+        for (int i = 0; i < str.length () - 1; i++) {
+            if ( (str.charAt (i) == 'y' || str.charAt (i) == 'z') && !Character.isLetter (str.charAt (i + 1)) ) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+}
